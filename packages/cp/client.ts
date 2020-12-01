f648dd75f004833a23411f112d590d21dd1ce440
estevam31 <estevamvinicius31@gmail.com>
2020-12-01 19:57:48 -0300

    test: fix open handles on tests

--- packages/cp/client.ts
+++ packages/cp/client.ts
@@ -1,21 +1,33 @@
-import { RequestHandler } from "../messages";
-import { CentralSystemAction, centralSystemActions } from "../messages/cs";
+import { RequestHandler } from '../messages';
+import { CentralSystemAction, centralSystemActions } from '../messages/cs';
 import WebSocket from 'ws';
-import { Connection, SUPPORTED_PROTOCOLS } from "../ws";
+import { Connection, SUPPORTED_PROTOCOLS } from '../ws';
 
 export default class ChargePoint {
+  private socket: WebSocket | undefined;
+  private socketConnected = false;
+
   constructor(
     private readonly cpId: string,
     private readonly requestHandler: RequestHandler<CentralSystemAction>,
-    private readonly csUrl: string,
-  ) { }
+    private readonly csUrl: string
+  ) {}
 
   connect() {
     const url = `${this.csUrl}/${this.cpId}`;
-    const socket = new WebSocket(url, SUPPORTED_PROTOCOLS);
+    this.socket = new WebSocket(url, SUPPORTED_PROTOCOLS);
 
-    const connection = new Connection(socket, this.requestHandler, centralSystemActions);
-    socket.on('error', console.error);
-    socket.on('message', data => connection.handleWebsocketData(data))
+    const connection = new Connection(
+      this.socket,
+      this.requestHandler,
+      centralSystemActions
+    );
+    this.socket.on('open', () => (this.socketConnected = true));
+    this.socket.on('error', console.error);
+    this.socket.on('message', (data) => connection.handleWebsocketData(data));
+  }
+
+  close() {
+    if (this.socketConnected) this.socket?.terminate();
   }
 }
--- packages/integration.spec.ts
+++ packages/integration.spec.ts
@@ -4,23 +4,27 @@ import { ChargePoint } from './cp';
 describe('test cs<->cp communication', () => {
   const PORT = 8080;
   const cs = new CentralSystem(PORT, (req, cpId) => {
-    return [{}, undefined]
+    return [{}, undefined];
   });
 
   const cp = new ChargePoint(
     '123',
-    req => [undefined, new Error('123')],
+    (req) => [undefined, new Error('123')],
     `ws://localhost:${PORT}`
   );
 
-  let triggerConnected = (cpId: string) => { };
+  afterAll(() => {
+    cs.close();
+    cp.close();
+  });
+
+  let triggerConnected = (cpId: string) => {};
   cs.addConnectionListener((cpId, status) => {
-    if (status === 'connected')
-      triggerConnected(cpId)
+    if (status === 'connected') triggerConnected(cpId);
   });
   const waitForConnection = (cpId: string) =>
-    new Promise(resolve => {
-      triggerConnected = connectedId => {
+    new Promise((resolve) => {
+      triggerConnected = (connectedId) => {
         if (connectedId == cpId) resolve(cpId);
       };
     });
@@ -28,6 +32,5 @@ describe('test cs<->cp communication', () => {
   it('should connect', async () => {
     cp.connect();
     await waitForConnection('123');
-  })
-
-})
+  });
+});
