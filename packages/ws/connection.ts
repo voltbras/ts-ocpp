a3f7ee6e170e190c4b98817d455a31cac228f6bb
eduhenke <eduardo_henke@hotmail.com>
2020-12-21 14:46:27 -0300

    Add send request to WS connection

--- packages/ws/connection.ts
+++ packages/ws/connection.ts
@@ -1,29 +1,60 @@
 import WebSocket from 'ws';
 import { parseOCPPMessage, stringifyOCPPMessage } from './format';
 import { MessageType, OCPPJMessage } from './types';
-import { Fail, Maybe, None, Success, Some } from 'monet';
-import { ActionName, RequestHandler, Response } from '../messages';
-import { OCPPApplicationError } from '../errors/index';
+import { Fail, Maybe, None, Success, Some, Validation } from 'monet';
+import { ActionName, Request, RequestHandler, Response } from '../messages';
+import { OCPPApplicationError, OCPPRequestError } from '../errors/index';
 import { validateMessage } from '../messages/validation';
+import * as uuid from 'uuid';
 
 export default class Connection<T extends ActionName> {
+  private messageTriggers: Record<string, (m: OCPPJMessage) => void> = {};
   constructor(
     private readonly socket: WebSocket,
     private readonly requestHandler: RequestHandler<T>,
     private readonly acceptedActions: T[],
   ) { }
 
+  public async sendRequest<T extends ActionName>(action: T, payload: Request<T>): Promise<Validation<OCPPRequestError, Response<T>>> {
+    const id = uuid.v4();
+    const waitResponse: Promise<OCPPJMessage> = new Promise(resolve => {
+      this.messageTriggers[id] = resolve;
+    });
+    await this.sendOCPPMessage({
+      id,
+      type: MessageType.CALL,
+      action,
+      payload,
+    })
+    const message = await waitResponse;
+
+    if (message.type === MessageType.CALL) return Fail(
+      new OCPPRequestError('response received was of CALL type, should be either CALLRESULT or CALLERROR')
+    );
+    if (message.type === MessageType.CALLERROR) return Fail(
+      new OCPPRequestError('other side responded with error', message.errorCode, message.errorDescription, message.errorDetails)
+    );
+    
+    return Success({ ...message.payload })
+  }
+
   public handleWebsocketData(data: WebSocket.Data) {
     parseOCPPMessage(data)
       .map(msg => this.handleOCPPMessage(msg))
       .forEach(result =>
-        result.forEach(response => {
-          this.socket.send(response)
-        })
+        result.forEach(response => this.sendOCPPMessage(response))
       )
   }
 
-  private handleOCPPMessage(message: OCPPJMessage): Maybe<string> {
+  private async sendOCPPMessage(message: OCPPJMessage): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.socket.send(stringifyOCPPMessage(message), err => {
+        err ? reject(err) : resolve()
+      })
+    })
+  }
+
+  private handleOCPPMessage(message: OCPPJMessage): Maybe<OCPPJMessage> {
     switch (message.type) {
       case MessageType.CALL:
         const response =
@@ -45,8 +76,15 @@ export default class Connection<T extends ActionName> {
               id: message.id,
               payload: success,
             }));
-        return Some(stringifyOCPPMessage(response));
+        return Some(response);
+      case MessageType.CALLERROR:
+      case MessageType.CALLRESULT:
+        this.messageTriggers[message.id](message);
     }
     return None();
+  }
+
+  public close() {
+    this.socket.close();
   }
 }
