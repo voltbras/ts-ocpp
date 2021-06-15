f2d9c010d684d05d81c0d81aa1c39aad7a09a96c
eduhenke <eduardo_henke@hotmail.com>
2021-06-15 01:16:35 -0300

    Fix OCPP v1.5 headers, add wsa5 headers

--- src/soap/connection.ts
+++ src/soap/connection.ts
@@ -6,6 +6,7 @@ import * as soap from 'soap';
 import * as path from 'path';
 import { chargePointActions } from '../messages/cp';
 import { soapCentralSystemActions } from '../messages/cs';
+import * as uuid from 'uuid';
 
 export default class SOAPConnection {
   private respondedActions: ActionName<'v1.5-soap'>[];
@@ -13,6 +14,7 @@ export default class SOAPConnection {
     private readonly soapClient: soap.Client,
     private readonly connectedTo: 'cp' | 'cs',
     private readonly chargePointId: string,
+    private readonly endpoint: string,
   ) {
     this.respondedActions = connectedTo === 'cp' ? soapCentralSystemActions : chargePointActions;
   }
@@ -24,7 +26,7 @@ export default class SOAPConnection {
         : '../messages/soap/ocpp_chargepointservice_1.5_final.wsdl'
     ));
     const soapClient = await soap.createClientAsync(wsdlPath, { endpoint, forceSoap12Headers: true });
-    return new SOAPConnection(soapClient, connectedTo, chargePointId);
+    return new SOAPConnection(soapClient, connectedTo, chargePointId, endpoint);
   }
 
   public sendRequest<T extends ActionName<'v1.5-soap'>>(action: T, { action: _, ocppVersion: __, ...payload }: Request<T, 'v1.5-soap'>): EitherAsync<OCPPRequestError, Response<T, 'v1.5-soap'>> {
@@ -33,8 +35,13 @@ export default class SOAPConnection {
       if (validateResult.isLeft())
         return Left(new OCPPApplicationError(validateResult.extract().toString()))
 
-      const xmlNs = this.connectedTo === 'cp' ? 'urn://Ocpp/Cs/2012/06/' : 'urn://Ocpp/Cp/2012/06/';
-      this.soapClient.addSoapHeader({ ChargeBoxIdentity: this.chargePointId }, '', '', xmlNs);
+      const xmlNs = this.connectedTo === 'cp' ? 'urn://Ocpp/Cp/2012/06/' : 'urn://Ocpp/Cs/2012/06/';
+      this.soapClient.addSoapHeader({ chargeBoxIdentity: this.chargePointId }, '', 'ocpp', xmlNs);
+      this.soapClient.addSoapHeader({
+        'Action': '/' + action,
+        'MessageID': uuid.v4(),
+        'To': this.endpoint,
+      }, '', 'wsa5', 'http://www.w3.org/2005/08/addressing');
 
       const [err, result, _rawResponse, _soapHeader, _rawRequest] = await new Promise(resolve => {
         const [serviceKey, portKey] = this.connectedTo === 'cp'
